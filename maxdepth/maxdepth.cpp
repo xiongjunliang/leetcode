aaa3c8a1b11d4fede94c6d0e16ac1e69966012a1
Xiongjun Liang <xiongjunliang@gmail.com>
2013-05-14 23:23:03 +0800

    add maxdepth

---
+++ maxdepth/maxdepth.cpp
@@ -0,0 +1,38 @@
+
+//  Max Depth Binary Tree 
+//  Author: Xiongjun Liang
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (root == NULL) {
+            return 0;
+        }
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return max(left, right) + 1;
+    }
+};
+
+
+int main() {
+
+    return 0;
+}
